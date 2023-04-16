04e9449bf5edd5ee0d7fe795388eedc37c11a6e6
Jane <janeluciaschoenfeld@gmail.com>
2023-04-16 11:36:44 +0200

    start filter

--- js/overview.js
+++ js/overview.js
@@ -1,10 +1,22 @@
 // Selectors
 const fewoList = document.querySelector('.fewo-list');
+const filterWrapper = document.querySelector('.all-filter-wrapper');
+const inputSearchName = document.querySelector('#search-for-name');
+const selectSearchNeighbourhood = document.querySelector(
+  '#neighbourhood-select'
+);
+const fewoTypeWrapper = document.querySelector('.fewo-type');
+const reviewScoreStart = document.querySelector('.review-score-start');
+const reviewScoreEnd = document.querySelector('.review-score-end');
+const priceStart = document.querySelector('.review-score-start');
+const priceEnd = document.querySelector('.review-score-end');
+const filterButton = document.getElementById('filter-btn');
 
 // Functions
 
 async function renderOverviewPage() {
   const details = (await csvData(listingCsvUrl)).data;
+  addFilterDetails(details);
   for (let i = 0; i <= 10; i++) {
     const fewo = details[i];
     addOverviewDetail(fewo, i);
@@ -22,12 +34,48 @@ function addDetail(tagName, className, parent) {
   return detail;
 }
 
+// add filter details
+
+function addFilterDetails(details) {
+  const neighbourhoods = [
+    ...new Set(details.map((d) => d.neighbourhood_cleansed).filter((d) => !!d)),
+  ].sort();
+
+  const roomType = [
+    ...new Set(details.map((d) => d.room_type).filter((d) => !!d)),
+  ];
+
+  neighbourhoods.map((n) => {
+    const optionNeighbourhood = addDetail(
+      'option',
+      null,
+      selectSearchNeighbourhood
+    );
+    optionNeighbourhood.setAttribute('value', n);
+    optionNeighbourhood.innerText = n;
+  });
+
+  roomType.map((t) => {
+    console.log(t);
+    const typeCheckbox = addDetail('input', null, fewoTypeWrapper);
+    typeCheckbox.setAttribute('type', 'checkbox');
+    typeCheckbox.setAttribute('id', `check-${t}`);
+    const labelCheckbox = addDetail('label', null, fewoTypeWrapper);
+    labelCheckbox.setAttribute('for', `check-${t}`);
+    labelCheckbox.innerText = t;
+  });
+
+  filterButton.addEventListener('click', () => filterFewo(details));
+}
+
 // add Overview Details
 
 function addOverviewDetail(fewo, i) {
   const fewoDiv = addDetail('div', 'fewo-list-item-wrapper', fewoList);
 
-  const fewoBild = addDetail('img', null, fewoDiv);
+  const fewoImageWrapper = addDetail('div', 'fewo-img-wrapper', fewoDiv);
+
+  const fewoBild = addDetail('img', null, fewoImageWrapper);
   fewoBild.setAttribute('src', fewo.picture_url);
 
   const fewoListInfo = addDetail('div', 'fewo-list-info', fewoDiv);
@@ -63,4 +111,26 @@ function addOverviewDetail(fewo, i) {
   const fewoDetailsLink = addDetail('a', 'details-bnt', fewoDiv);
   fewoDetailsLink.innerText = 'mehr anzeigen';
   fewoDetailsLink.setAttribute('href', `./about.html?id=${i}`);
+}
+
+function filterFewo(details) {
+  console.log(details);
+
+  // Name der Unterkunft
+  const filteredDetails = details
+    .filter((d) => d.name)
+    .filter((d) => d.name.includes(inputSearchName.value));
+  // Stadtteil
+  // Typ der Unterkunft
+  // Bewertung
+  // Preis
+  // Sortierung Bewertung
+  // Preis Bewertung
+
+  // filteredDetails.map((m) => m.name).forEach(console.log);
+  // filteredDetails.forEach((d) => {
+  //   console.log(d.name);
+  // });
+
+  // console.log(filteredDetails);
 }
